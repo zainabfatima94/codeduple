53827e25eafdfa03a77ffd6e405c4147370ea792
Zainab <zainabfatima13569@gmail.com>
2025-10-03 15:03:28 +0000

    commited more project setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import ChatBox from './components/ChatBox'
+import Message from './components/Message'
+import Credits from './page/Credits'
+import Community from './page/Community'
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <>
+      <div className='dark:bg-gradient-to-b from[#242124] to-[#000000] dark:text-white'>
+        <div className='flex h-screen w-screen'>
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+
+          </Routes>
+        </div>
+      </div>
+    </>
   )
 }
+
+export default App
